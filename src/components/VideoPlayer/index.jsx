f5741afcfdaec4f41f7b7f5784ad352cc14e8ff3
ozkr <oscarmenacho23@gmail.com>
2022-01-23 20:23:49 -0500

    feat(Root): Show first video on TikTok Clone

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,11 +1,30 @@
+import { useRef, useState } from 'react'
 import styles from './styles.module.css'
 
-const SRC = "https://v16-webapp.tiktok.com/c103c6d0a5303e564d9604ea4c63f900/61ec13a5/video/tos/useast2a/tos-useast2a-ve-0068c002/64017f94d00249f6a666038ddc63d4d9/?a=1988&amp;br=2030&amp;bt=1015&amp;cd=0%7C0%7C0%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;dr=0&amp;ds=2&amp;er=&amp;ft=Yu12_FGgkag3-I&amp;l=202201220824270102231280870E9BBF6F&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=am5rcjs8ZHNtNjMzNzczM0ApOGk7NTtkZjw6NzU5OjQ1Nmcuc2RpM2leMDNgLS1kMTZzczY0Yl8wNGJhNjUzYWJgMy06Yw%3D%3D&amp;vl=&amp;vr=";
-
+const SRC = 'https://v16-webapp.tiktok.com/83633c778d767ee8146f106148bfe21d/61ee3614/video/tos/alisg/tos-alisg-pve-0037c001/7434915487c649fbab35b5b78afb7a19/?a=1988&amp;br=3850&amp;bt=1925&amp;cd=0%7C0%7C1%7C0&amp;ch=0&amp;cr=0&amp;cs=0&amp;cv=1&amp;dr=0&amp;ds=3&amp;er=&amp;ft=XOQ9-3E7nz7ThN995lXq&amp;l=202201232314170101910410190CC02832&amp;lr=tiktok_m&amp;mime_type=video_mp4&amp;net=0&amp;pl=0&amp;qs=0&amp;rc=M3k8bDk6ZnJvOTMzODczNEApZzppZDdkaWU2N2RnZTw3PGdgaHNhcjRnLnNgLS1kMS1zczY0MjAzMy80LV9gMTUyNGE6Yw%3D%3D&amp;vl=&amp;vr='
 
 export default function VideoPlayer () {
-    return (
-        <video className={styles.video} src={SRC} controls={false} />
-    )
-}
+  const [playing, setPlaying] = useState(false)
+  const video = useRef()
+  const handlePlay = () => {
+    if (playing) {
+      video.current.pause()
+    } else {
+      video.current.play()
+    }
 
+    setPlaying(!playing)
+  }
+
+  return (
+    <div>
+      <video
+        className={styles.video}
+        src={SRC}
+        controls={false}
+        ref={video}
+      />
+      <button className={styles.player} onClick={handlePlay} />
+    </div>
+  )
+}
